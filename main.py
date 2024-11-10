db0ad6bbf542c719d0ff11402a32d1e97b67c9e5
Nazifatul Fadhilah <nazifatulfadhilah@gmail.com>
2024-11-10 23:18:16 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from math import sqrt
+from tabulate import tabulate
+
+class Membership:
+# data member
+  user_data = {
+      "Sumbul": "Platinum",
+      "Ana": "Gold",
+      "Cahya": "Platinum"
+  }
+# data membership
+  membership = {
+      'Membership': ['Platinum','Gold','Silver'],
+      'Discount' : ['15%','10%','8%'],
+      'Another Benefit' : ["Benefit Gold + Silver + Cashback max. 30%","Benefit Silver + Voucher Ojek Online","Voucher Makanan"]
+      }
+
+  requirements = {
+      'Membership': ['Platinum','Gold','Silver'],
+      'Monthly Expense (juta)' : [8,6,5],
+      'Monthly Income (juta)': [15,10,7]
+}
+  def __init__(self,username):
+    """
+    Fungsi ini untuk menginisiasi objek user
+
+    input: username (str)
+    """
+    self.username = username
+  
+  def show_benefit(self):
+    """
+    Fungsi ini untuk menampilkan all membership benefit
+    """
+
+    print(f"Benefit Membership PacCommerce\n")
+    print(tabulate(self.membership,headers='keys',stralign='center'))
+
+    # print(f"{self.headers}")
+    # print('-'*50)
+    # for row in range(len(self.list_benefit)):
+    #   print(self.list_benefit[row])
